259ef9d6be671749bc2096be33a17c83505c5be5
Tamara Zaira Santos de Almeida <TamaraZaira@MacBook-Pro.local>
2023-04-07 21:14:11 -0300

    Design pattern

---
+++ src/entities/form/Shape.java
@@ -0,0 +1,15 @@
+package entities.form;
+
+import intefaces.Color;
+
+public abstract class Shape {
+    //Composition - implementor
+    protected Color color;
+
+    //constructor with implementor as input argument
+    public Shape(Color c){
+        this.color=c;
+    }
+
+    abstract public void applyColor();
+}
